cb0541b544098da394648aea22d2836fb733551b
DenisSpatari <103810113+DenisSpatari@users.noreply.github.com>
2023-03-15 12:00:18 +0200

    Mascota

---
+++ Site_Component/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult blank()
+        {
+            return View();
+        }
+        public ActionResult checkout()
+        {
+            return View();
+        }
+        public ActionResult product()
+        {
+            return View();
+        }
+        public ActionResult store()
+        {
+            return View();
+        }
+    }
+}
---
+++ Site_Component/WebApplication1/js/mascot.js
@@ -0,0 +1,4 @@
+﻿document.querySelector('.messenger').classList.add('tilt-in-fwd-br');
+setTimeout(() => {
+     document.querySelector('.messenger').classList.add('slide-out-bck-center');
+}, 3000);
